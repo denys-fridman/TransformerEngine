64db10d0334f38b7edb342ace32242939625cd75
PerfClaw <group_174004_bot_perfclaw@noreply.gitlab-master.nvidia.com>
2026-05-03 13:08:20 +0000

    iter6: depth_to_max_tensors[1] 64->110 (clip multi_tensor_scale/adam pack more tensors per launch, kernel arg under 4KB)

--- transformer_engine/common/multi_tensor/multi_tensor_apply.cuh
+++ transformer_engine/common/multi_tensor/multi_tensor_apply.cuh
@@ -15,7 +15,7 @@
 // This header is the one-stop shop for all your multi-tensor apply needs.
 
 // TODO:  Kernel arg size limit may be <4KB for some other cards (ie Jetson)
-constexpr int depth_to_max_tensors[6] = {110, 64, 48, 36, 30, 24};
+constexpr int depth_to_max_tensors[6] = {110, 110, 48, 36, 30, 24};
 constexpr int depth_to_max_blocks[6] = {320, 320, 320, 320, 320, 320};
 
 template <int n, bool USE_FP8 = false>
